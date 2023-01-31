c469d5d20d32ee9c2545654229dc7aac1b107092
macbook <seha_malkocoglu@hotmail.com>
2023-01-30 20:27:30 -0500

    new changes

---
+++ src/Day1GitHub/secondGitHubClass.java
@@ -0,0 +1,8 @@
+package Day1GitHub;
+
+public class secondGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("hello b7");
+        System.out.println("1+1");
+    }
+}
